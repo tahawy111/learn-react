9100bab36d079166f5c45396460f7cd79c26f154
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-04-15 00:36:33 +0200

    k

--- src/component/CompA.jsx
+++ src/component/CompA.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const CompA = () => {
-  return <div></div>;
+  // https://jsonplaceholder.typicode.com/posts
+  const [id, setId] = useState(0);
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  return (
+    <div className="">
+      <input type="number" onChange={(e) => setId(e.target.value)} />
+
+      <ul>
+        <li>
+          {post.id} - {post.title}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default CompA;
