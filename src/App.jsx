425c72ea9940fd669553e6d7fc8b14e929ce4caf
TAHAWY <amer.zp582@gmail.com>
2022-04-15 15:32:31 +0200

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React, { Component } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import "./App.css";
-import Sidebar from "./component/Sidebar";
-import Widget from "./component/Widget";
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Sidebar />
-        <Widget />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [user, setUser] = useState("");
+  const refInput = useRef();
+  const count = useRef(0);
+  useEffect(() => {
+    count.current = count.current + 1;
+  }, [user]);
+
+  return (
+    <div>
+      <h1 className="text-secondary">
+        Hooks
+        <strong className="text-danger">
+          [ <span className="text-primary">useRef</span> ]
+        </strong>
+      </h1>
+      <input
+        ref={refInput}
+        type="text"
+        onChange={(e) => setUser(e.target.value)}
+      />
+      {user}
+      <br />
+      {count.current}
+    </div>
+  );
+};
 
 export default App;
